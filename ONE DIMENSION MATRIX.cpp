22cfeae964b2475934119a9056779bada3f4f33b
NkathaKirimi <mnkatha28@gmail.com>
2025-03-09 19:26:32 +0300

    project added

---
+++ ONE DIMENSION MATRIX.cpp
@@ -0,0 +1,20 @@
+NAME;MERCY NKATHA
+BSCIT-05-0047\2024
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[] = {1, 2, 3, 4, 5};
+    
+    // Accessing elements
+    cout << matrix[0] << endl;  // Output: 1
+
+    // Iterating over the array
+    for(int i = 0; i < 5; i++) {
+        cout << matrix[i] << " ";
+    }
+
+    return 0;
+}
